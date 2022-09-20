44735855fbb5a108749b31353385b62eadcc9b21
JieMin <jiemind@outlook.com>
2022-09-20 19:34:27 +0800

    Update 1-init-key.sh

--- 1-init-key.sh
+++ 1-init-key.sh
@@ -1,6 +1,6 @@
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa -q
-cat ~/.ssh/id_rsa >> ~/key.pem
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa > ~/key.pem
+cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
 echo "Please enter your sudo Password. >"
 read -r sudopw
 echo "&sudopw" | sudo -S sh -c "cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ucf-old"
