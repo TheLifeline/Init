1c70a94530015c8401e40b0e95524334d992dd2e
JieMin <jiemind@outlook.com>
2022-09-20 19:04:12 +0800

    Update 1-init-key.sh

--- 1-init-key.sh
+++ 1-init-key.sh
@@ -1,13 +1,14 @@
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa -q
 cat ~/.ssh/id_rsa >> ~/key.pem
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ucf-old 
-sudo sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g"  /etc/ssh/sshd_config
-sudo sh -c 'echo "
+echo "Please enter your sudo Password "
+read -r SUDO_PASSWORD
+echo &SUDO_PASSWORD | sudo -S sh -c "cp /etc/ssh/sshd_config /etc/ssh/sshd_config.ucf-old"
+echo &SUDO_PASSWORD | sudo -S sh -c 'sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g"  /etc/ssh/sshd_config'
+echo &SUDO_PASSWORD | sudo -S sh -c 'echo "
 RSAAuthentication yes
 PubkeyAuthentication yes
 PasswordAuthentication no
 PrintMotd yes
 " >> /etc/ssh/sshd_config.d/my.config'
-sudo service sshd restart
+echo &SUDO_PASSWORD | sudo -S sh -c 'service sshd restart'
