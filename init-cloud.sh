0a35de1a7b7a72d5f76f6a98f796d8a19116d2c2
TheLifeline <jiemind@outlook.com>
2022-10-07 23:12:33 +0800

    add centos

--- init-cloud.sh
+++ init-cloud.sh
@@ -3,6 +3,10 @@ passwd ubuntu<<EOF
 pass
 pass
 EOF
+passwd centos<<EOF
+pass
+pass
+EOF
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 # echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/my.config
 # sed -i -e 's/.*exit 142" \(.*$\)/\1/' /root/.ssh/authorized_keys
