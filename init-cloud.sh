8c63fd9f7cf6f7a6636e18b1a61731acf227d801
TheLifeline <jiemind@outlook.com>
2022-10-18 18:53:33 +0800

    modify

--- init-cloud.sh
+++ init-cloud.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+passwd root<<EOF
+password
+password
+EOF
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+# sed -i -e 's/.*;exit 142" \(.*$\)/\1/' /root/.ssh/authorized_keys
+service sshd restart
+
 passwd ubuntu<<EOF
 password
 password
@@ -7,10 +16,3 @@ passwd centos<<EOF
 password
 password
 EOF
-passwd root<<EOF
-password
-password
-EOF
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-# sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
-# sed -i -e 's/.*;exit 142" \(.*$\)/\1/' /root/.ssh/authorized_keys
