d5b105a4be05c6ae75275713a7944b31794ba25f
JieMin <jiemind@outlook.com>
2023-04-21 11:29:03 +0800

    Update 2-init-ubuntu.sh

--- 2-init-ubuntu.sh
+++ 2-init-ubuntu.sh
@@ -18,10 +18,10 @@ echo "&sudopw" | sudo -S apt install -y git wget build-essential apt-transport-h
 
 #install docker
 echo "&sudopw" | sudo -S apt-get remove docker docker-engine docker.io containerd runc
-echo "&sudopw" | sudo -S sh -c 'wget -qO- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor > docker-archive-keyring.gpg'
-echo "&sudopw" | sudo -S install -o root -g root -m 644 docker-archive-keyring.gpg /etc/apt/trusted.gpg.d/
-echo "&sudopw" | sudo -S sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
-echo "&sudopw" | sudo -S sh -c 'rm -f docker-archive-keyring.gpg'
+echo "&sudopw" | sudo -S install -m 0755 -d /etc/apt/keyrings
+echo "&sudopw" | sudo -S sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
+echo "&sudopw" | sudo -S chmod a+r /etc/apt/keyrings/docker.gpg
+echo "&sudopw" | sudo -S sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
 echo "&sudopw" | sudo -S apt-get update 
 echo "&sudopw" | sudo -S apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
