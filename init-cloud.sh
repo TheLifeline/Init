c006f5a8e4f411ab57857943014dc9cb41093187
JieMin <jiemind@outlook.com>
2024-11-22 17:20:10 +0800

    Update init-cloud.sh

--- init-cloud.sh
+++ init-cloud.sh
@@ -3,7 +3,8 @@ passwd root<<EOF
 password
 password
 EOF
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+rm /etc/ssh/sshd_config.d/*
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config.d/60-my-cloudimg-settings.conf
 # sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 # sed -i -e 's/.*;exit 142" \(.*$\)/\1/' /root/.ssh/authorized_keys
 service sshd restart
